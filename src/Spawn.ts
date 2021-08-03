28058f2cebb50d1937503e226ae31b5419edc5b3
Julian <julian.blazek@outlook.de>
2021-08-03 22:36:29 +0200

    Balances out plans so creeps are spawned equally

--- src/Spawn.ts
+++ src/Spawn.ts
@@ -2,25 +2,36 @@ import IBuildingPlan from "IBuildingPlan"
 import { Role } from "Roles"
 import { bodycost } from "Util"
 
-const harvester:IBuildingPlan = {
-    configurations: [[WORK, CARRY, MOVE, MOVE], [WORK, WORK, CARRY, MOVE, MOVE], [WORK, WORK, CARRY, CARRY, MOVE, MOVE]],
+const harvester: IBuildingPlan = {
+    configurations: [[WORK, CARRY, MOVE, MOVE], [WORK, WORK, CARRY, MOVE, MOVE], [WORK, WORK, CARRY, CARRY, MOVE, MOVE], [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]],
     role: Role.HARVESTER,
     desired: 5
 }
 
-const builder:IBuildingPlan = {
+const builder: IBuildingPlan = {
     configurations: [[WORK, CARRY, MOVE, MOVE], [WORK, WORK, CARRY, MOVE, MOVE], [WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE]],
     role: Role.BUILDER,
     desired: 5
 }
-const buildingPlans:IBuildingPlan[] = [harvester, builder]
+const buildingPlans: IBuildingPlan[] = [harvester, builder]
 
 // Sort the plans by descending cost
 buildingPlans.forEach(plan => { plan.configurations.sort((a, b) => bodycost(b) - bodycost(a)) })
 
+function sortPlansByDesireRatio(plans: Array<IBuildingPlan>): IBuildingPlan[] {
+    const creepsByRole = Object.keys(Game.creeps).reduce((acc, name) => {
+        let role = Game.creeps[name].memory.role
+        acc[role] = (acc[role] || []).concat(Game.creeps[name])
+        return acc;
+    }, {} as { [key in Role]: Creep[] })
+    return plans.sort((a, b) => {
+        return (creepsByRole[a.role]?.length / a.desired) - (creepsByRole[b.role]?.length / b.desired)
+    })
+}
+
 export function spawnCreeps() {
     let availableEnergy = Game.spawns['Spawn1'].room.energyAvailable
-    buildingPlans.forEach(plan => {
+    sortPlansByDesireRatio(buildingPlans).forEach((plan, _index) => {
         let config = plan.configurations.find(conf => bodycost(conf) <= availableEnergy)
         if (config) { // Config buildable with current resources found
             let count = Object.keys(Game.creeps).filter(key => Game.creeps[key].memory.role == plan.role).length
@@ -31,13 +42,6 @@ export function spawnCreeps() {
             }
         }
     })
-
-    /*const upgraders= _.filter(Game.creeps, creep => creep.memory.role == 'upgrader')
-    if (upgraders.length < 1) {
-        let upgraderName = 'Upgrader' + Game.time
-        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], upgraderName,
-            {memory: {role: 'upgrader', atWork: true}})
-    }*/
 }
 
 export function visual() {
