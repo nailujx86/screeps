c1c860fc0c397717ea425af6c8bf74c960a1e8fc
Julian <julian.blazek@outlook.de>
2021-08-03 22:35:50 +0200

    Add memory to structures

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,51 @@ import profiler from "screeps-profiler"
 
 declare global {
     interface CreepMemory extends ICreepMemory {}
+    interface StructureMemory {
+        needsRepair?: boolean
+    }
+    interface OwnedStructure {
+        memory: StructureMemory
+    }
+    interface Memory {
+        structures: {[id: string]: StructureMemory}
+    }
+    // spawn creep code
+}
+
+function patchPrototypes() {
+    if (!Memory.structures) {
+        Memory.structures = {}
+    }
+    Object.defineProperty(OwnedStructure.prototype, 'memory', {
+        get: function() {
+            return Memory.structures[this.id]
+        },
+        set: function(value) {
+            Memory.structures[this.id] = value
+        },
+        configurable: true
+    })
 }
 
 //profiler.enable()
 export const loop = () => {
+    patchPrototypes()
     profiler.wrap(function() {
         if (Game.cpu.bucket == 10000) {
             Game.cpu.generatePixel()
             console.log("Pixel generated!")
         }
-
+        
         for (let name in Memory.creeps) {
             if (!(name in Game.creeps))
                 delete Memory.creeps[name]
         }
-        spawner.spawnCreeps()
+
+        if(Game.time % 5 == 0) {
+            spawner.spawnCreeps()
+        }
+        
         spawner.visual()
 
         for(let name in Game.creeps) {
